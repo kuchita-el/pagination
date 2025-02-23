652a982454a8f5c425f07d981a214385196c5532
Kuchita El <el.kuchita@gmail.com>
2025-02-23 02:46:07 +0000

    ブログ一覧を取得する処理を関数化

--- src/app/components/BlogList.tsx
+++ src/app/components/BlogList.tsx
@@ -7,14 +7,21 @@ export function BlogList() {
     const [cursor, setCursor] = useState<string>();
     const [formerCursors, setFormerCursors] = useState<string[]>([]);
 
-    useEffect(() => {
-        (async () => {
-            const _c = formerCursors[formerCursors.length - 1];
-            const response = await fetch(`/api/blogs?cursor=${_c}`)
-            const blogs = await response.json() as { id: string }[]
-            setCursor(blogs[blogs.length - 1].id);
-        })()
+    const fetchBlogs = async (cursor?: string) => {
+        const response = await fetch(`/api/blogs?cursor=${cursor}`)
+        const blogs = await response.json() as { id: string }[]
+        return {
+            blogs: blogs,
+            cursor: blogs[blogs.length - 1].id
+        }
+    }
 
+    useEffect(() => {
+        const _cursor = formerCursors.length === 0 ? undefined : formerCursors[formerCursors.length - 1];
+        fetchBlogs(_cursor)
+            .then(data => {
+                setCursor(data.cursor);
+            })
         return () => { }
     }, [formerCursors])
 
